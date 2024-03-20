588fa57157c24f9cd1ca9ab2e4b5aa6238611b2e
Paul Qi <qhnmgzg@gmail.com>
2024-03-20 01:08:06 -0400

    debug (runnable)

--- 2_run.py
+++ 2_run.py
@@ -15,13 +15,15 @@ import subprocess
 def write_paths_to_csv(root_dir, csv_dir, script_dir, origin_dir):
     """
     Writes paths of .jpeg images located in '1.0' subfolders of directories ending with '_files'
-    to a CSV file, overwriting the file for each directory found. Executes the feature extractor then. 
+    to a CSV file. Executes the feature extractor then. 
     
     :param root_dir: Path to the directory containing folders ending with '_files'.
     :param csv_dir: Path to the CSV file to write the image paths to.
     :param script_dir: Path to the directory of the feature extractor. 
     :param origin_dir: Path to this current working directory. 
     """
+    all_image_paths = [] 
+
     for item in os.listdir(root_dir):
         folder_path = os.path.join(root_dir, item)
         if os.path.isdir(folder_path) and item.endswith('_files'):
@@ -31,15 +33,26 @@ def write_paths_to_csv(root_dir, csv_dir, script_dir, origin_dir):
                     os.path.join(subfolder_path, img) for img in os.listdir(subfolder_path)
                     if img.endswith('.jpeg')
                 ]
-                with open(csv_dir, 'w', newline='') as csvfile:
-                    writer = csv.writer(csvfile)
-                    for path in image_paths:
-                        writer.writerow([path])
+                all_image_paths.extend(image_paths)
 
-            os.chdir(script_dir)
-            subprocess.call(['python', 'run.py'])
-            os.chdir(origin_dir)
-            break
+            #     with open(csv_dir, 'w', newline='') as csvfile:
+            #         writer = csv.writer(csvfile)
+            #         for path in image_paths:
+            #             writer.writerow([path])
+
+            # os.chdir(script_dir)
+            # subprocess.call(['python', 'run.py'])
+            # os.chdir(origin_dir)
+
+    if all_image_paths:
+        with open(csv_dir, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for path in all_image_paths:
+                writer.writerow([path])
+    
+    os.chdir(script_dir)
+    subprocess.call(['python', 'run.py'])
+    os.chdir(origin_dir)
 
 
 patches_dir = "/Users/paul/Desktop/EC500_GTP_For_WSI/patch"
