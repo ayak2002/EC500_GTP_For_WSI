32ac425b96a7ca89ee3be4167f7beb00bddf95d7
Paul Qi <qhnmgzg@gmail.com>
2024-03-19 22:18:54 -0400

    add feature extractor code

---
+++ 2_feature_extractor/run.py
@@ -0,0 +1,31 @@
+"""
+    Usage:
+        python run.py
+
+        Add the folder path containing the patches to all_patches.csv first. 
+
+    Input: 
+        Patches
+    Output:
+        ???
+"""
+
+from simclr import SimCLR
+import yaml
+from data_aug.dataset_wrapper import DataSetWrapper
+import os, glob
+import pandas as pd
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--magnification', type=str, default='20x')
+    args = parser.parse_args()
+    config = yaml.load(open("config.yaml", "r"), Loader=yaml.FullLoader)
+    dataset = DataSetWrapper(config['batch_size'], **config['dataset'])
+    
+    simclr = SimCLR(dataset, config)
+    simclr.train()
+
+if __name__ == "__main__":
+    main()
